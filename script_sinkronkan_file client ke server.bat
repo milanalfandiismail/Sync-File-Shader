202ef6464edd3e5f2bc1f7497a952dd4e6821999
milanalfandiismail <110679364+milanalfandiismail@users.noreply.github.com>
2025-01-29 17:15:47 +0800

    Update script_sinkronkan_file client ke server.bat
    update

--- script_sinkronkan_file client ke server.bat
+++ script_sinkronkan_file client ke server.bat
@@ -1,16 +1,39 @@
 @echo off
-REM Ganti dengan path folder jaringan tujuan
-set NETWORK_PATH="\\MILANGAMECENTER"  
+
+setlocal enabledelayedexpansion
+
+REM ========== BAGIAN DAFTAR IP YANG DIPILIH ==========
+REM Edit IP-IP berikut sesuai dengan kebutuhan Anda
+set ip1=10.10.10.10
+set ip2=10.10.10.9
+set ip3=10.10.10.8
+
+REM Tentukan jumlah total IP yang tersedia
+set IP_COUNT=3
+REM ========== END BAGIAN DAFTAR IP ==========
+
+REM ========== PEMILIHAN IP RANDOM ==========
+set /a random_index=%RANDOM% %% %IP_COUNT% + 1
+set selected_ip=!ip%random_index%!
+
+REM ========== SET SERVER PATH ==========
+set SERVER_PATH=\\%selected_ip%
+
+echo Menggunakan IP server: %selected_ip%
+echo Server path: %SERVER_PATH%
+echo.
 
 REM Mengambil nama komputer
 set COMPUTER_NAME=%COMPUTERNAME%
 
 REM Membuat folder baru dengan nama komputer dan subfolder yang diperlukan
-set NEW_FOLDER=\\MILANGAMECENTER\%COMPUTER_NAME%
+set NEW_FOLDER=%SERVER_PATH%\%COMPUTER_NAME%
 set NEW_FOLDER1=%NEW_FOLDER%\DxCache
 set NEW_FOLDER2=%NEW_FOLDER%\DxcCache
 set NEW_FOLDER3=%NEW_FOLDER%\D3DSCache
 set NEW_FOLDER4=%NEW_FOLDER%\Delta_Force
+set NEW_FOLDER5=%NEW_FOLDER%\Wuthering_Waves
+set NEW_FOLDER6=%NEW_FOLDER%\Apex
 
 REM Memeriksa apakah folder tujuan sudah ada dan membuat folder baru jika tidak ada
 if not exist %NEW_FOLDER% (
@@ -41,24 +64,41 @@ if not exist %NEW_FOLDER4% (
     mkdir %NEW_FOLDER4%%
 )
 
+if not exist %NEW_FOLDER5% (
+    echo Membuat subfolder Wuthering_Waves
+    mkdir %NEW_FOLDER5%
+)
+
+if not exist %NEW_FOLDER6% (
+    echo Membuat subfolder Apex
+    mkdir %NEW_FOLDER6%
+)
+
 REM Menyalin file dari folder lokal ke folder jaringan untuk masing-masing subfolder
 set SOURCE_DIR1=%localappdata%\AMD\DxCache
 set SOURCE_DIR2=%localappdata%\AMD\DxcCache
 set SOURCE_DIR3=%localappdata%\D3DSCache
 set SOURCE_DIR4=G:\Steam\steamapps\common\Delta Force\Game\DeltaForce\Saved
-
+set SOURCE_DIR5=G:\Wuthering Waves\Wuthering Waves Game\Client\Saved\PSO
+set SOURCE_DIR6=%USERPROFILE%\Saved Games\Respawn\Apex\local
 
 REM Salin file dari DxCache ke folder tujuan
-robocopy "%SOURCE_DIR1%" "%NEW_FOLDER1%" /E /Z /XO
+robocopy "%SOURCE_DIR1%" "%NEW_FOLDER1%" /E /Z /XO /W:5
 
 REM Salin file dari DxcCache ke folder tujuan
-robocopy "%SOURCE_DIR2%" "%NEW_FOLDER2%" /E /Z /XO
+robocopy "%SOURCE_DIR2%" "%NEW_FOLDER2%" /E /Z /XO /W:5
 
 REM Salin file dari D3DSCache ke folder tujuan
-robocopy "%SOURCE_DIR3%" "%NEW_FOLDER3%" /E /Z /XO
+robocopy "%SOURCE_DIR3%" "%NEW_FOLDER3%" /E /Z /XO /W:5
 
-REM Salin file dari Delta_Force ke folder tujuan
-robocopy "%SOURCE_DIR4%" "%NEW_FOLDER4%" /E /Z /XO
+REM Salin file Shader Delta Force
+robocopy "%SOURCE_DIR4%" "%NEW_FOLDER4%" *.ushaderprecache /E /Z /XO /W:5
+
+REM Salin file shader Wuthering_Waves
+robocopy "%SOURCE_DIR5%" "%NEW_FOLDER5%" /E /Z /XO /W:5
+
+REM Salin file shader Apex
+robocopy "%SOURCE_DIR6%" "%NEW_FOLDER6%" *.pso /E /Z /XO /W:5
+
 
 echo Transfer selesai.
-
